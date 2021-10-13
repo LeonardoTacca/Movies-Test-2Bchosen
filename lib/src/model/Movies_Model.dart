6f19a11803dde1d9f77c066c9dc8f7ff58261c9d
LeonardoTacca <leotacca@hotmail.com>
2021-10-13 15:00:03 -0300

    Correção Model Filmes por categoria + api corrigida

--- lib/src/model/Movies_Model.dart
+++ lib/src/model/Movies_Model.dart
@@ -1,42 +1,68 @@
 class MoviesList {
-  late String description;
-  late int favoriteCount;
+  late bool adult;
+  late String backdropPath;
+  late List<int> genreIds;
   late int id;
-  late int itemCount;
-  late String iso6391;
-  late String listType;
-  late String name;
+  late String originalLanguage;
+  late String originalTitle;
+  late String overview;
+  late double popularity;
+  late String posterPath;
+  late String releaseDate;
+  late String title;
+  late bool video;
+  late double voteAverage;
+  late int voteCount;
 
-  MoviesList({
-    required this.description,
-    required this.favoriteCount,
-    required this.id,
-    required this.itemCount,
-    required this.iso6391,
-    required this.listType,
-    required this.name,
-  });
+  MoviesList(
+      {required this.adult,
+      required this.backdropPath,
+      required this.genreIds,
+      required this.id,
+      required this.originalLanguage,
+      required this.originalTitle,
+      required this.overview,
+      required this.popularity,
+      required this.posterPath,
+      required this.releaseDate,
+      required this.title,
+      required this.video,
+      required this.voteAverage,
+      required this.voteCount});
 
   MoviesList.fromJson(Map<String, dynamic> json) {
-    description = json['description'];
-    favoriteCount = json['favorite_count'];
+    adult = json['adult'];
+    backdropPath = json['backdrop_path'];
+    genreIds = json['genre_ids'].cast<int>();
     id = json['id'];
-    itemCount = json['item_count'];
-    iso6391 = json['iso_639_1'];
-    listType = json['list_type'];
-    name = json['name'];
+    originalLanguage = json['original_language'];
+    originalTitle = json['original_title'];
+    overview = json['overview'];
+    popularity = json['popularity'];
+    posterPath = json['poster_path'];
+    releaseDate = json['release_date'];
+    title = json['title'];
+    video = json['video'];
+    voteAverage = json['vote_average'];
+    voteCount = json['vote_count'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['description'] = this.description;
-    data['favorite_count'] = this.favoriteCount;
+    data['adult'] = this.adult;
+    data['backdrop_path'] = this.backdropPath;
+    data['genre_ids'] = this.genreIds;
     data['id'] = this.id;
-    data['item_count'] = this.itemCount;
-    data['iso_639_1'] = this.iso6391;
-    data['list_type'] = this.listType;
-    data['name'] = this.name;
-
+    data['original_language'] = this.originalLanguage;
+    data['original_title'] = this.originalTitle;
+    data['overview'] = this.overview;
+    data['popularity'] = this.popularity;
+    data['poster_path'] = this.posterPath;
+    data['release_date'] = this.releaseDate;
+    data['title'] = this.title;
+    data['video'] = this.video;
+    data['vote_average'] = this.voteAverage;
+    data['vote_count'] = this.voteCount;
     return data;
   }
 }
